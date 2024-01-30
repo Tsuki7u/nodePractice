237b398b54e47fffcb256cdbd744c03e6244cbb8
Tsuki7u <llarrh4@gmail.com>
2024-01-30 16:24:09 +0800

    feat: 实现单例模式

--- index.js
+++ index.js
@@ -50,105 +50,73 @@ const filterFiles = files
     },
   }));
 
-let CosSingle = (function () {
-  let instance;
-  return function (name) {
-    if (instance) {
-      return instance;
-    }
-    this.name = name;
-    return (instance = this);
-  };
-})();
-CosSingle.prototype.getName = function () {
-  return this.name;
-};
-let cosConfig = new CosSingle(COS);
-console.log(cosConfig,'cosConfig');
-let cosConfigSetting = cosConfig.getName().getAuthorization;
-console.log(cosConfigSetting, "cosConfig111");
-// cosConfig1(function (options, callback) {
-//   // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
-//   // 异步获取临时密钥
-//   request(
-//     {
-//       url: "https://example.com/sts", // 替换为自己的获取临时密钥的服务url
-//       data: {
-//         // 可从 options 取需要的参数
-//       },
-//     },
-//     function (err, response, body) {
-//       let data = null;
-//       let credentials = null;
-//       try {
-//         data = JSON.parse(body);
-//         credentials = data.credentials;
-//       } catch (e) {}
-//       if (!data || !credentials) return console.error("credentials invalid");
-//       callback({
-//         TmpSecretId: credentials.tmpSecretId, // 临时密钥的 tmpSecretId
-//         TmpSecretKey: credentials.tmpSecretKey, // 临时密钥的 tmpSecretKey
-//         SecurityToken: credentials.sessionToken, // 临时密钥的 sessionToken
-//         // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
-//         StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
-//         ExpiredTime: data.expiredTime, // 临时密钥失效时间戳，是申请临时密钥时，时间戳加 durationSeconds
-//       });
-//     }
-//   );
-// });
-// cosConfig.getName().getAuthorization(options, callback) {}
-// cosConfig.getAuthorization();
-
-// const cos = new COS({
-//   getAuthorization: function (options, callback) {
-//     // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
-//     // 异步获取临时密钥
-//     request(
-//       {
-//         url: "https://example.com/sts", // 替换为自己的获取临时密钥的服务url
-//         data: {
-//           // 可从 options 取需要的参数
-//         },
-//       },
-//       function (err, response, body) {
-//         let data = null;
-//         let credentials = null;
-//         try {
-//           data = JSON.parse(body);
-//           credentials = data.credentials;
-//         } catch (e) {}
-//         if (!data || !credentials) return console.error("credentials invalid");
-//         callback({
-//           TmpSecretId: credentials.tmpSecretId, // 临时密钥的 tmpSecretId
-//           TmpSecretKey: credentials.tmpSecretKey, // 临时密钥的 tmpSecretKey
-//           SecurityToken: credentials.sessionToken, // 临时密钥的 sessionToken
-//           // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
-//           StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
-//           ExpiredTime: data.expiredTime, // 临时密钥失效时间戳，是申请临时密钥时，时间戳加 durationSeconds
-//         });
-//       }
-//     );
-//   },
-// });
-
-function cosUploadFiles() {
-  cosConfigSetting.uploadFiles(
-    {
-      files: filterFiles,
-      SliceSize: 1024 * 1024 * 10 /* 设置大于10MB采用分块上传 */,
-      onProgress: function (info) {
-        let percent = parseInt(info.percent * 10000) / 100;
-        let speed = parseInt((info.speed / 1024 / 1024) * 100) / 100;
-        console.log("进度：" + percent + "%; 速度：" + speed + "Mb/s;");
-        progress.run(percent);
+class CosUploader {
+  constructor() {
+    // 在这里初始化 COS 对象
+    this.cos = new COS({
+      getAuthorization: function (options, callback) {
+        // 初始化时不会调用，只有调用 cos 方法（例如 cos.putObject）时才会进入
+        // 异步获取临时密钥
+        request(
+          {
+            url: "https://example.com/sts", // 替换为自己的获取临时密钥的服务url
+            data: {
+              // 可从 options 取需要的参数
+            },
+          },
+          function (err, response, body) {
+            let data = null;
+            let credentials = null;
+            try {
+              data = JSON.parse(body);
+              credentials = data.credentials;
+            } catch (e) {}
+            if (!data || !credentials)
+              return console.error("credentials invalid");
+            callback({
+              TmpSecretId: credentials.tmpSecretId, // 临时密钥的 tmpSecretId
+              TmpSecretKey: credentials.tmpSecretKey, // 临时密钥的 tmpSecretKey
+              SecurityToken: credentials.sessionToken, // 临时密钥的 sessionToken
+              // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+              StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+              ExpiredTime: data.expiredTime, // 临时密钥失效时间戳，是申请临时密钥时，时间戳加 durationSeconds
+            });
+          }
+        );
       },
-      onFileFinish: function (err, data, options) {
-        console.log(options.Key + "上传" + (err ? "失败" : "完成"));
-      },
-    },
-    function (err, data) {
-      console.log(err || data);
+    });
+  }
+
+  static getInstance() {
+    if (!CosUploader.instance) {
+      CosUploader.instance = new CosUploader();
     }
-  );
+    return CosUploader.instance;
+  }
+
+  uploadFile() {
+    this.cos.uploadFiles(
+      {
+        files: filterFiles,
+        SliceSize: 1024 * 1024 * 10 /* 设置大于10MB采用分块上传 */,
+        onProgress: function (info) {
+          let percent = parseInt(info.percent * 10000) / 100;
+          let speed = parseInt((info.speed / 1024 / 1024) * 100) / 100;
+          console.log("进度：" + percent + "%; 速度：" + speed + "Mb/s;");
+          progress.run(percent);
+        },
+        onFileFinish: function (err, data, options) {
+          console.log(options.Key + "上传" + (err ? "失败" : "完成"));
+        },
+      },
+      function (err, data) {
+        console.log(err || data);
+      }
+    );
+  }
 }
-cosUploadFiles();
+const cosUploader1 = CosUploader.getInstance();
+const cosUploader2 = CosUploader.getInstance();
+console.log(cosUploader1 === cosUploader2); // 输出 true，说明是同一个实例
+
+cosUploader1.uploadFile();
